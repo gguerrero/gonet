ad2532e855574fcca21879059528fccbc6734ca9
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2021-09-21 21:23:42 +0400

    Error handling in HTTP parse and test

--- netplexer/request/http_parser_test.go
+++ netplexer/request/http_parser_test.go
@@ -6,22 +6,32 @@ import (
 	"testing"
 )
 
-var requestText = bufio.NewReader(strings.NewReader(`
-GET /foo HTTP1.1
-Content-Type:text/html
-
-`))
+var requestText = bufio.NewReader(
+	strings.NewReader(
+		"GET /foo HTTP1.1\r\n" +
+			"Host: http://example.com\r\n" +
+			"Content-Type: text/html\r\n" +
+			"\r\n"))
 
 func TestParse(t *testing.T) {
 	req := new(HttpRequest)
 	parser := HttpParser{requestText}
 
-	t.Fatal("nOOOO")
-	t.Log("Test parser.Parse(req)")
+	t.Log("Given an HTTP I/O to parse")
+
 	err := parser.Parse(req)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("Parse failed!", err)
 	}
 
-	t.Log(req)
+	t.Log("it parses the Method\t")
+	t.Log(req.Method)
+	if req.Method != "GET" {
+		t.Fatal("Fail!")
+	}
+
+	t.Log("it parses the Host\t")
+	if req.Host != "http://example.com" {
+		t.Fatal("Fail!")
+	}
 }
